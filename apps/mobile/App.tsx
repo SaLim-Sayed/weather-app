28f86295527198b366fa19b3d7c8320e28a83f1b
Salem Sayed <salemsayed981@gmail.com>
2025-08-23 10:46:44 +0300

    setup : mobile app

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,25 @@
+
+// eslint-disable-next-line import/no-unresolved
+import { ScreenContent } from 'components/ScreenContent';
+import { StatusBar } from 'expo-status-bar';
+
+
+
+
+import './global.css';
+
+
+import { ImageBackground } from 'react-native';
+
+
+export default function App() {
+  return (
+       <ImageBackground source={require('./assets/bg.png')} className="flex-1 items-center justify-center">
+        <ScreenContent title="Home" path="App.tsx">
+        </ScreenContent>
+        <StatusBar style="auto" />
+      </ImageBackground>
+  );
+}
+
+
